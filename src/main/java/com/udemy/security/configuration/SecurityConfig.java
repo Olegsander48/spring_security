830e6e4304b71ae1fdd6578fe89fd4a0c1761031
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-09-30 17:56:14 +0300

    store password in database id dcrypt-format

--- src/main/java/com/udemy/security/configuration/SecurityConfig.java
+++ src/main/java/com/udemy/security/configuration/SecurityConfig.java
@@ -5,43 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails ivan = User.builder()
-                .username("ivan")
-                .password(encoder.encode("1234"))
-                .roles("EMPLOYEE")
-                .build();
+    private final DataSource dataSource;
 
-        UserDetails elena = User.builder()
-                .username("elena")
-                .password(encoder.encode("1234"))
-                .roles("HR")
-                .build();
-
-        UserDetails andrey = User.builder()
-                .username("andrey")
-                .password(encoder.encode("1234"))
-                .roles("HR", "MANAGER")
-                .build();
-
-        return new InMemoryUserDetailsManager(ivan, elena, andrey);
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public UserDetailsService userDetailsService() {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
