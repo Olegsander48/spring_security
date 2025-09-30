157d396bc5d593e776ab122cbc266ea8acb06221
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-09-30 17:27:59 +0300

    added authorization to app

--- src/main/java/com/udemy/security/configuration/SecurityConfig.java
+++ src/main/java/com/udemy/security/configuration/SecurityConfig.java
@@ -2,13 +2,16 @@ package com.udemy.security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -39,5 +42,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(customizer -> customizer
+                        .requestMatchers("/hr_info").hasRole("HR")
+                        .requestMatchers("/manager_info/**").hasRole("MANAGER")
+                        .requestMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER"))
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .build();
     }
 }
